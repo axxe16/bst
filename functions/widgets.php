a11e8cd53b85985c43cf3d94418d7a76b8b89a42
Alessandro Grassi <grassi.alssandro@gmail.com>
2015-07-23 17:54:49 +0200

    aggiunta di 3 sidebar per la homepage

--- functions/widgets.php
+++ functions/widgets.php
@@ -3,6 +3,45 @@
 function bst_widgets_init() {
 
   /*
+  widget 1 home
+   */
+  register_sidebar( array(
+    'name'            => __( 'Widget home 1', 'bst' ),
+    'id'              => 'widget-home1',
+    'description'     => __( 'Widget 1 homepage', 'bst' ),
+    'before_widget'   => '<section class="%1$s %2$s">',
+    'after_widget'    => '</section>',
+    'before_title'    => '<h4>',
+    'after_title'     => '</h4>',
+  ) );
+
+  /*
+  widget 2 home
+   */
+  register_sidebar( array(
+    'name'            => __( 'Widget home 2', 'bst' ),
+    'id'              => 'widget-home2',
+    'description'     => __( 'Widget 2 homepage', 'bst' ),
+    'before_widget'   => '<section class="%1$s %2$s">',
+    'after_widget'    => '</section>',
+    'before_title'    => '<h4>',
+    'after_title'     => '</h4>',
+  ) );
+
+  /*
+  widget 3 home
+   */
+  register_sidebar( array(
+    'name'            => __( 'Widget home 3', 'bst' ),
+    'id'              => 'widget-home3',
+    'description'     => __( 'Widget 3 homepage', 'bst' ),
+    'before_widget'   => '<section class="%1$s %2$s">',
+    'after_widget'    => '</section>',
+    'before_title'    => '<h4>',
+    'after_title'     => '</h4>',
+  ) );    
+
+  /*
   Sidebar (one widget area)
    */
   register_sidebar( array(
