3977bef3c092c18aba99b1a5b69d1c841f6075af
Alessandro Grassi <grassi.alssandro@gmail.com>
2015-10-14 19:20:52 +0200

    varie

--- front-page.php
+++ front-page.php
@@ -1,20 +1,12 @@
 <?php get_template_part('includes/header'); ?>
-
 <div class="container">
-
-<?php if ( is_active_sidebar( 'widget-home1' ) || is_active_sidebar( 'widget-home2' ) || is_active_sidebar( 'widget-home3' )) { ?>
- <div class="row"> 
-   <div class="col-xs-12 col-sm-4">
-	   <?php dynamic_sidebar('widget-home1'); ?>
+<?php if ( is_active_sidebar( 'widget-home-section-1' )) { ?>
+   <div class="row">
+   		<?php dynamic_sidebar('widget-home-section-1'); ?>
    </div>
-   <div class="col-xs-12 col-sm-4">
-	   <?php dynamic_sidebar('widget-home2'); ?>
-   </div>
-   <div class="col-xs-12 col-sm-4">
-	   <?php dynamic_sidebar('widget-home3'); ?>
-   </div>
-</div>
 <?php } ?>
+
+
 
   <div class="row">
     
--- functions/widgets.php
+++ functions/widgets.php
@@ -3,43 +3,19 @@
 function bst_widgets_init() {
 
   /*
-  widget 1 home
+  widget section 1 home
    */
   register_sidebar( array(
-    'name'            => __( 'Widget home 1', 'bst' ),
-    'id'              => 'widget-home1',
-    'description'     => __( 'Widget 1 homepage', 'bst' ),
+    'name'            => __( 'widget home section 1', 'bst' ),
+    'id'              => 'widget-home-section-1',
+    'description'     => __( 'widget home section 1', 'bst' ),
     'before_widget'   => '<section class="%1$s %2$s">',
     'after_widget'    => '</section>',
     'before_title'    => '<h4>',
     'after_title'     => '</h4>',
   ) );
 
-  /*
-  widget 2 home
-   */
-  register_sidebar( array(
-    'name'            => __( 'Widget home 2', 'bst' ),
-    'id'              => 'widget-home2',
-    'description'     => __( 'Widget 2 homepage', 'bst' ),
-    'before_widget'   => '<section class="%1$s %2$s">',
-    'after_widget'    => '</section>',
-    'before_title'    => '<h4>',
-    'after_title'     => '</h4>',
-  ) );
 
-  /*
-  widget 3 home
-   */
-  register_sidebar( array(
-    'name'            => __( 'Widget home 3', 'bst' ),
-    'id'              => 'widget-home3',
-    'description'     => __( 'Widget 3 homepage', 'bst' ),
-    'before_widget'   => '<section class="%1$s %2$s">',
-    'after_widget'    => '</section>',
-    'before_title'    => '<h4>',
-    'after_title'     => '</h4>',
-  ) );    
 
   /*
   Sidebar (one widget area)
